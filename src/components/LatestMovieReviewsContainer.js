8c829fcc05b486f79317f5d7f6ff101c6063b183
Luis Castillo <mr.luiscastillo@gmail.com>
2021-09-18 23:39:33 -0400

    commit tree 5bb75e495bc08eb900b8a6aa72e6ea7500a6251d

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,8 +2,24 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'your-key-here';
+const NYT_API_KEY = 'XD8mzv9CyiJWYcH2zMNMyrb1bbG2PVLZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+class LatestMovieReviewsContainer extends Component {
+    state = { reviews: [] };
+  
+    render() {
+      return <div className='latest-movie-reviews'><MovieReviews reviews={this.state.reviews} /></div>;
+    }
+    
+    componentDidMount() {
+      fetch(URL)
+      .then(res => res.json())
+      .then(json => this.setState({ reviews: json.results }))
+    }
+  }
+
+  export default LatestMovieReviewsContainer;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,33 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'your-key-here';
+const NYT_API_KEY = 'XD8mzv9CyiJWYcH2zMNMyrb1bbG2PVLZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
+class SearchableMovieReviewsContainer extends Component {
+    state = { 
+      reviews: [],
+      searchTerm: ''
+    }
+  
+    render() {
+      return (
+        <div className='searchable-movie-reviews'>
+          <form>
+            <input type='text' />
+          </form>
+          <MovieReviews reviews={this.state.reviews} />
+        </div>
+      )
+    }
+  
+    componentDidMount() {
+      fetch(URL)
+      .then(res => res.json())
+      .then(json => this.setState({ reviews: json.results }))
+    }
+  }
+  export default SearchableMovieReviewsContainer;
